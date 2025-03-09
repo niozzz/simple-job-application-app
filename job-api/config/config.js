b2957e099419c304108fd06d2039c42e644c0579
Millenio Rachmat Prayogo <rachmatjackson@gmail.com>
2025-03-09 17:08:10 +0700

    feat : add rate limiter

--- job-api/config/config.js
+++ job-api/config/config.js
@@ -11,7 +11,13 @@ module.exports = {
     logging: false,
   },
   test: {
-    // test environment config
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+    logging: false,
   },
   production: {
     // production environment config
--- job-api/server.js
+++ job-api/server.js
@@ -3,9 +3,16 @@ const express = require("express");
 const cors = require("cors");
 const routes = require("./routes");
 const db = require("./models");
-
+const rateLimit = require("express-rate-limit");
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per windowMs
+});
+
+app.use(limiter);
 
 // Middleware
 app.use(cors());
@@ -34,3 +41,5 @@ process.on("unhandledRejection", (err) => {
   console.error(err.name, err.message);
   process.exit(1);
 });
+
+module.exports = app;
